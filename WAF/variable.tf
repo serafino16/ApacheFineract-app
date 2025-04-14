a8898f4e913f6d14261cc4d167aa9fe26040af5b
Serafino Stoimenov <40893656+serafino16@users.noreply.github.com>
2025-04-14 16:02:45 +0300

    Update variable.tf

--- WAF/variable.tf
+++ WAF/variable.tf
@@ -1,7 +1,7 @@
 
 provider "aws" {
-  alias  = "us-east-1"
-  region = "us-east-1"
+  alias  = "us-west-2"
+  region = "us-west-2"
 }
 
 provider "aws" {
@@ -10,8 +10,8 @@ provider "aws" {
 }
 
 provider "aws" {
-  alias  = "eu-central-1"
-  region = "eu-central-1"
+  alias  = "us-west-3"
+  region = "us-west-3"
 }
 
 
@@ -22,9 +22,9 @@ variable "waf_environments" {
     enable   = bool
   }))
   default = {
-    devstage = { region = "us-east-1", alb_arn = "arn:aws:elasticloadbalancing:us-east-1:account-id:loadbalancer/app/devstage-alb-id", enable = true }
+    devstage = { region = "us-west-3", alb_arn = "arn:aws:elasticloadbalancing:us-east-1:account-id:loadbalancer/app/devstage-alb-id", enable = true }
     prod     = { region = "us-west-1", alb_arn = "arn:aws:elasticloadbalancing:us-west-1:account-id:loadbalancer/app/prod-alb-id", enable = true }
-    prodbackup = { region = "eu-central-1", alb_arn = "arn:aws:elasticloadbalancing:eu-central-1:account-id:loadbalancer/app/prodbackup-alb-id", enable = true }
+    prodbackup = { region = "us-west-2", alb_arn = "arn:aws:elasticloadbalancing:eu-central-1:account-id:loadbalancer/app/prodbackup-alb-id", enable = true }
   }
 }
 
@@ -32,8 +32,8 @@ variable "waf_environments" {
 variable "region_providers" {
   type = map(string)
   default = {
-    us-east-1    = "aws.us-east-1"
+    us-east-1    = "aws.us-west-3"
     us-west-1    = "aws.us-west-1"
-    eu-central-1 = "aws.eu-central-1"
+    eu-central-1 = "aws.us-west-2"
   }
 }
